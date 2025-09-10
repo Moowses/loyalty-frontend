5a99427361f81f68264936abfd344bf5fe6932c8
Moowses <132836745+Moowses@users.noreply.github.com>
2025-09-10 15:56:30 -0400

    Update SiteHeader.tsx

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -208,7 +208,7 @@ async function logout() {
         >
           HELP
           <Image
-            src="/Navhelp.png"
+            src="/navhelp.png"
             alt="Help"
             width={21}   // 5x5 px (Tailwind 5 = 1.25rem ≈ 20px)
             height={21}
